511b57109def9ba4f878cad9caf066a717c42f84
Sangram Thapa <thapasangram64@gmail.com>
2025-01-02 14:00:02 +0545

    handberg menu remaining

--- my_interface/src/Nav.jsx
+++ my_interface/src/Nav.jsx
@@ -1,8 +1,61 @@
-
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
+import "./App.css";
 function Nav() {
   return (
-    <div>Nav</div>
-  )
+    <>
+      <nav className="Navigation">
+        <div className="name">
+          <h2>
+            <spam>S</spam>angram
+            <spam>T</spam>hapa
+          </h2>
+        </div>
+        <div className="menu_link">
+          <ul>
+            <li>
+              <a href="#">HOME</a>
+            </li>
+            <li>
+              <a href="#">ABOUT</a>
+            </li>
+            <li>
+              <a href="#">CONTACT</a>
+            </li>
+            <li>
+              <a href="#">SERVCES</a>
+            </li>
+          </ul>
+        </div>
+        <div className="social-media">
+          <ul>
+            <li>
+              <a href="https://www.facebook.com/profile.php?id=61551788280821&mibextid=ZbWKwL">
+                <FaFacebookSquare />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FaInstagramSquare />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FaGithubSquare />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="hamburger-menu">
+          <a href="#">
+            <RxHamburgerMenu />
+          </a>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Nav
+export default Nav;
